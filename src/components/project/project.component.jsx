ee21b7b6544396aa725dfb24568e034c951c1fe7
unknown <ericsshyus@gmail.com>
2021-10-23 05:42:47 -0400

    added all sections to the main page

--- src/components/project/project.component.jsx
+++ src/components/project/project.component.jsx
@@ -4,7 +4,7 @@ import github from "../../assets/pictures/github-icon.svg";
 
 const Project = ({ name, description, githubLink, link, live }) => (
   <div className="project">
-    <div>{name}</div>
+    <div className="title">{name}</div>
     <img className="project-image" src={link} alt="project preview"></img>
 
     <p>{description}</p>
@@ -14,7 +14,13 @@ const Project = ({ name, description, githubLink, link, live }) => (
           <img className="github-icon" src={github} alt="github"></img>
         </a>
       </li>
-      <li>live website link</li>
+      {live ? (
+        <li>
+          <a href={live} target="_blank" rel="noreferrer">
+            live
+          </a>
+        </li>
+      ) : null}
     </ul>
   </div>
 );
--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import "./projects.styles.scss";
 import Project from "../project/project.component";
 import onlineGarageSale from "../../assets/pictures/online-garage-sale.png";
-
+import genshinCompanion from "../../assets/pictures/genshin-companion.png";
 const projects = [
   {
     name: "online garage sale",
     github: "https://github.com/pongpwner/online-garage-sale",
     description: "ecommerce website",
     link: onlineGarageSale,
-    live: "",
+
+    id: 1,
+  },
+  {
+    name: "Genshin Impact Companion",
+    github: "https://github.com/pongpwner/genshin-calculator",
+    description: "companion web app for the game Genshin Impact",
+    link: genshinCompanion,
+    live: "https://genshin-impact-companion.herokuapp.com/",
+    id: 2,
   },
 ];
 
@@ -19,6 +28,7 @@ const Projects = () => (
     <div className="project-list">
       {projects.map((project) => (
         <Project
+          key={project.id}
           name={project.name}
           description={project.description}
           githubLink={project.github}
