aa0b5d7c281b2adbbc489c1045010ebf8b360118
unknown <ericsshyus@gmail.com>
2021-10-27 02:47:58 -0400

    added mobile navbar

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.styles.scss";
 
-const Header = () => (
-  <header className="header">
-    <ul className="list-container">
-      <li>About</li>
-      <li>Skills</li>
-      <li>Projects</li>
-      <li>Contact</li>
-    </ul>
-  </header>
-);
+const Header = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  function toggleNavbar() {
+    setNavbar(!navbar);
+  }
+
+  return (
+    <header className="header">
+      <div className="burger" onClick={toggleNavbar}>
+        <div className="burger-line"></div>
+        <div className="burger-line"></div>
+        <div className="burger-line"></div>
+      </div>
+      <ul className={`list-container ${navbar ? "dflex" : "dnone"}`}>
+        <li onClick={() => setNavbar(false)}>About</li>
+        <li onClick={() => setNavbar(false)}>Skills</li>
+        <li onClick={() => setNavbar(false)}>Projects</li>
+        <li onClick={() => setNavbar(false)}>Contact</li>
+      </ul>
+    </header>
+  );
+};
 
 export default Header;
