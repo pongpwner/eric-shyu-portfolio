71365a17620c9a81d78a0ed45cd04d9e4cd951a8
pongpwner <ericsshyus@gmail.com>
2022-02-17 04:10:01 -0500

    added modal for project, and resized project section

--- src/components/project/project.component.jsx
+++ src/components/project/project.component.jsx
@@ -1,31 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./project.styles.scss";
+import Modal from "../modal/modal.component";
 import github from "../../assets/pictures/github-icon.svg";
 
-const Project = ({ name, description, githubLink, link, live }) => (
-  <div className="project">
-    <div className="title">{name}</div>
-    <a href={live} target="_blank" rel="noreferrer" className="link-container">
-      <img className="project-image" src={link} alt="project preview"></img>
-    </a>
-    <div className="group1">
-      <p>{description}</p>
-      <ul className="links">
-        <li>
-          <a href={githubLink} target="_blank" rel="noreferrer">
-            <img className="github-icon" src={github} alt="github"></img>
-          </a>
-        </li>
-        {live ? (
+const Project = ({
+  name,
+  description,
+  githubLink,
+  link,
+  live,
+  modalDescription,
+}) => {
+  const [active, setActive] = useState(false);
+  function toggleModal() {
+    setActive(!active);
+  }
+
+  return (
+    <div className="project" onClick={toggleModal}>
+      <Modal active={active} closeModal={toggleModal}>
+        <img className="project-image" src={link} alt="project preview"></img>
+        {modalDescription}
+        <ul className="links">
           <li>
-            <a href={live} target="_blank" rel="noreferrer">
-              live
+            <a href={githubLink} target="_blank" rel="noreferrer">
+              <img className="github-icon" src={github} alt="github"></img>
             </a>
           </li>
-        ) : null}
-      </ul>
+          {live ? (
+            <li>
+              <a href={live} target="_blank" rel="noreferrer">
+                live
+              </a>
+            </li>
+          ) : null}
+        </ul>
+      </Modal>
+      <div className="title">{name}</div>
+
+      <img className="project-image" src={link} alt="project preview"></img>
+
+      <div className="group1">
+        <p>{description}</p>
+        <ul className="links">
+          <li>
+            <a href={githubLink} target="_blank" rel="noreferrer">
+              <img className="github-icon" src={github} alt="github"></img>
+            </a>
+          </li>
+          {live ? (
+            <li>
+              <a href={live} target="_blank" rel="noreferrer">
+                live
+              </a>
+            </li>
+          ) : null}
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Project;
